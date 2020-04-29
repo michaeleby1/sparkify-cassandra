58a099ccc130353c82cf996a1f3780bc3489f688
Michael Eby <michaeleby1@gmail.com>
2020-04-29 15:12:05 -0400

    new .py files for etl pipeline and cql queries

---
+++ cql_queries.py
@@ -0,0 +1,12 @@
+# ---------------------- #
+#  DROP TABLES           #
+# ---------------------- #
+
+drop_song_library = "DROP TABLE IF EXISTS song_library"
+drop_user_library = "DROP TABLE IF EXISTS user_library"
+drop_music_app_history = "DROP TABLE IF EXISTS music_app_history"
+
+
+# ---------------------- #
+#  DROP TABLES           #
+# ---------------------- #
